492184d56414fda742ef9f8820665242ccbd4ffc
Sayyid <sayyid.gibran212@gmail.com>
2025-02-20 16:19:28 +0700

    Mengubah Latar Belakang

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,20 @@
 import "@/global.css";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {/* Mengarahkan ke Layar Home */}
+      <TouchableOpacity
+        activeOpacity={0.8}
+        className="bg-green-900/70 px-4 py-2 rounded-md"
+        onPress={() => router.push("/screens/home")}
+      >
+        <Text className="text-white font-bold">Masuk Beranda</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx},",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#131415",
+      },
+    },
   },
   plugins: [],
 };
