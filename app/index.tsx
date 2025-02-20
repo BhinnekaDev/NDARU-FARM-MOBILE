84448071a5ebb062d68c943083435a6cda06f773
fifovalle <fifanaufal10@gmail.com>
2025-02-20 16:17:07 +0700

    Merubah latar belakang

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
 import "@/global.css";
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {/* Mengarahkan Ke Halaman Register */}
+      <TouchableOpacity onPress={() => router.push("/screens/register")}>
+        <Text>Menuju Halaman Register</Text>
+      </TouchableOpacity>
     </View>
   );
 }
---
+++ app/screens/register.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Register() {
+  return (
+    <View className="flex-1 justify-center items-center bg-background">
+      <Text>Register</Text>
+    </View>
+  );
+}
