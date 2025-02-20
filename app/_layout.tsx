39b6ec67fd8131a1b1bd4651af07fddd73f66f6a
Sayyid <sayyid.gibran212@gmail.com>
2025-02-20 16:24:49 +0700

    Mengubah Warna Latar Status Bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }} />
+      <StatusBar barStyle="light-content" backgroundColor="#131315" />
+    </>
+  );
 }
