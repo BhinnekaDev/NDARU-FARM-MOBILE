8e2f77f244237a18033ea82df3a1c259a11563e6
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-03-08 22:51:03 +0700

    feat: Menambahkan router ke Profile

--- app/Profile/index.tsx
+++ app/Profile/index.tsx
@@ -1,19 +1,18 @@
+import "@/global.css";
 import { Text, View } from "react-native";
 import { useRouter } from "expo-router";
 import Button from "@/components/Elements/Button"; // Sesuaikan path jika berbeda
 
 export default function Index() {
   const router = useRouter();
-
   const handleNavigation = () => {
-    router.push("/Profile"); // Sesuaikan dengan nama folder
+    router.push("/"); // Sesuaikan dengan nama folder
   };
-
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl">Welcome Everyone 👋</Text>
-      <Button className="bg-red-500" onPress={handleNavigation}>
-        Go To Profile Dawg
+      <Text className="text-2xl">Welcome To Profile 👋</Text>
+      <Button className="bg-green-500" onPress={handleNavigation}>
+        Go To Home Dawg
       </Button>
     </View>
   );
