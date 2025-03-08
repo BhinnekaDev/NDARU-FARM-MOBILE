b72ce3887315bc967197ef63ef395f4c348ec532
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-03-09 00:26:06 +0700

    feat: mengubah halaman home

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,20 @@
-import "@/global.css";
 import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import Button from "@/components/Elements/Button";
 
 export default function Index() {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    router.push("/Profile");
+  };
+
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-2xl">Welcome Everyone 👋</Text>
+      <Button className="bg-red-500" onPress={handleNavigation}>
+        Go To Profile Dawg
+      </Button>
     </View>
   );
 }
