c70f82b5fdfd7bde3346be47a94cb54dcf0c51b3
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-03-14 12:18:14 +0700

    feat: menambahkan 4 fitur Tabs Bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,47 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
-
-const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
     <Tabs
       screenOptions={({ route }) => {
-        const iconName = route.name === "Profile" ? "person-outline" : "home-outline"; // Default ke "home-outline" jika tidak cocok
+        let iconName: keyof typeof Ionicons.glyphMap;
+
+        switch (route.name) {
+          case "home":
+            iconName = "home-outline";
+            break;
+          case "cart":
+            iconName = "cart-outline";
+            break;
+          case "favorite":
+            iconName = "star-outline";
+            break;
+          case "profile":
+            iconName = "person-outline";
+            break;
+          default:
+            iconName = "ellipse-outline";
+        }
 
         return {
           tabBarIcon: ({ color, size }) => (
             <Ionicons
-              name={iconName as keyof typeof Ionicons.glyphMap} //
+              name={iconName} //
               size={size}
               color={color}
             />
           ),
           tabBarActiveTintColor: "#6200EE",
-          tabBarInactiveTintColor: "red",
+          tabBarInactiveTintColor: "gray",
           headerShown: false,
         };
       }}
     >
       <Tabs.Screen name="home" />
+      <Tabs.Screen name="cart" />
+      <Tabs.Screen name="favorite" />
       <Tabs.Screen name="profile" />
     </Tabs>
   );
