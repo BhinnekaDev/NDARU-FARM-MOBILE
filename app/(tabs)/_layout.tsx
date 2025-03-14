8b079bbb814fb6930e06675a2c7260c76c8f8993
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-03-14 12:57:57 +0700

    refactor: perbaiki ketika bar di tap, warna berubah

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,19 +7,24 @@ export default function TabNavigator() {
     <Tabs
       screenOptions={({ route }) => {
         let iconName: keyof typeof Ionicons.glyphMap;
+        let labelName: string = route.name.charAt(0).toUpperCase() + route.name.slice(1); // Kapitalisasi huruf pertama
 
         switch (route.name) {
           case "home":
             iconName = "home-outline";
+            labelName = "Home";
             break;
           case "cart":
             iconName = "cart-outline";
+            labelName = "Cart";
             break;
           case "favorite":
             iconName = "star-outline";
+            labelName = "Favorite";
             break;
           case "profile":
             iconName = "person-outline";
+            labelName = "Profile";
             break;
           default:
             iconName = "ellipse-outline";
@@ -33,16 +38,26 @@ export default function TabNavigator() {
               color={color}
             />
           ),
-          tabBarActiveTintColor: "#6200EE",
+          tabBarActiveTintColor: "white",
           tabBarInactiveTintColor: "gray",
           headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "black",
+            height: 60,
+            borderTopWidth: 1,
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+            fontWeight: "600",
+          },
+          tabBarLabel: labelName,
         };
       }}
     >
-      <Tabs.Screen name="home" />
-      <Tabs.Screen name="cart" />
-      <Tabs.Screen name="favorite" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen name="home" options={{ tabBarLabel: "Home" }} />
+      <Tabs.Screen name="cart" options={{ tabBarLabel: "Cart" }} />
+      <Tabs.Screen name="favorite" options={{ tabBarLabel: "Favorites" }} />
+      <Tabs.Screen name="profile" options={{ tabBarLabel: "Profile" }} />
     </Tabs>
   );
 }
