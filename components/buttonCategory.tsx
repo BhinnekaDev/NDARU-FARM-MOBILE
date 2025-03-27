51103857eff2bcc478a8642ecc1edf12424e6ed5
Sayyid <sayyid.gibran212@gmail.com>
2025-03-27 22:13:59 +0700

    feat: menambahkan animasi scroll otomatis pada kategori

--- components/buttonCategory.tsx
+++ components/buttonCategory.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { ScrollView, TouchableOpacity, useColorScheme } from "react-native";
 // COMPONENTS
 import MyText from "@/components/text";
@@ -13,9 +13,29 @@ const MyButtonCategory: React.FC<MyButtonCategoryProps> = ({
 }) => {
   const colorScheme = useColorScheme();
   const isDarkMode = colorScheme === "dark";
+  const scrollRef = useRef<ScrollView>(null);
+  const buttonRefs = useRef<
+    React.RefObject<React.ElementRef<typeof TouchableOpacity>>[]
+  >(
+    categories.map(() =>
+      React.createRef<React.ElementRef<typeof TouchableOpacity>>()
+    )
+  );
+
+  const handleCategoryPress = (category: string, index: number) => {
+    onSelectCategory(category);
+
+    buttonRefs.current[index].current?.measureLayout(
+      scrollRef.current as any,
+      (x: number) => {
+        scrollRef.current?.scrollTo({ x, animated: true });
+      }
+    );
+  };
 
   return (
     <ScrollView
+      ref={scrollRef}
       horizontal
       showsHorizontalScrollIndicator={false}
       className="flex-row"
@@ -26,7 +46,8 @@ const MyButtonCategory: React.FC<MyButtonCategoryProps> = ({
         return (
           <TouchableOpacity
             key={index}
-            onPress={() => onSelectCategory(category)}
+            ref={buttonRefs.current[index]}
+            onPress={() => handleCategoryPress(category, index)}
             activeOpacity={0.8}
             className={`px-4 py-2 rounded-full ${
               isSelected ? (isDarkMode ? "bg-[#333836]" : "bg-[#093731]") : ""
