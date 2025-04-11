9581b1d13122bf0ca084710f8327e5266c1e4d69
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-04-11 15:55:09 +0700

    feat: menambahkan fitur keyboard ketika scroll

--- app/screens/editAddress.tsx
+++ app/screens/editAddress.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, useColorScheme } from "react-native";
+import { View, KeyboardAvoidingView, ScrollView, Platform, Keyboard, TouchableWithoutFeedback, useColorScheme } from "react-native";
 import { useRouter } from "expo-router";
 
 // OUR COMPONENTS
@@ -25,79 +25,95 @@ export default function editAddressScreen() {
   const [address, setAddress] = useState("");
 
   return (
-    <View className={`flex-1 ${isDarkMode ? "bg-black" : "bg-white"}`}>
-      {/* Header Button Kembali */}
-      <HeaderBackButton
-        onPress={() => router.push("/screens/editProfile")} //
-        title="Sunting Nama Lengkap"
-      />
+    <KeyboardAvoidingView
+      className={`flex-1 ${isDarkMode ? "bg-black" : "bg-white"}`} //
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className={`flex-1 ${isDarkMode ? "bg-black" : "bg-white"}`}>
+          {/* Header Button Kembali */}
+          <HeaderBackButton
+            onPress={() => router.push("/screens/editProfile")} //
+            title="Sunting Nama Lengkap"
+          />
+          <View className="flex-1 ">
+            <ScrollView
+              contentContainerStyle={{
+                paddingVertical: 10,
+                flexGrow: 1,
+              }} //
+              keyboardShouldPersistTaps="handled"
+            >
+              {/*Form*/}
+              <View className="flex justify-center items-center py-6">
+                {/* Form Nama Lengkap*/}
+                <FloatingLabelInput
+                  label="Provinsi" //
+                  value={province}
+                  onChangeText={(input) => setProvince(nameFormatter(input))}
+                />
 
-      {/*Form*/}
-      <View className="flex justify-center items-center  py-6">
-        {/* Form Nama Lengkap*/}
-        <FloatingLabelInput
-          label="Provinsi" //
-          value={province}
-          onChangeText={(input) => setProvince(nameFormatter(input))}
-        />
+                {/* Form Kota*/}
+                <FloatingLabelInput
+                  label="Kota" //
+                  value={city}
+                  onChangeText={(input) => setCity(nameFormatter(input))}
+                />
 
-        {/* Form Kota*/}
-        <FloatingLabelInput
-          label="Kota" //
-          value={city}
-          onChangeText={(input) => setCity(nameFormatter(input))}
-        />
+                {/* Form Kabupaten*/}
+                <FloatingLabelInput
+                  label="Kabupaten" //
+                  value={regency}
+                  onChangeText={(input) => setRegency(nameFormatter(input))}
+                />
 
-        {/* Form Kabupaten*/}
-        <FloatingLabelInput
-          label="Kabupaten" //
-          value={regency}
-          onChangeText={(input) => setRegency(nameFormatter(input))}
-        />
+                {/* Form Postal Code*/}
+                <FloatingLabelInput
+                  label="Postal Code" //
+                  value={numberPostalCode}
+                  onChangeText={(input) => setNumberPostalCode(numberFormatter(input, 6))}
+                />
 
-        {/* Form Postal Code*/}
-        <FloatingLabelInput
-          label="Postal Code" //
-          value={numberPostalCode}
-          onChangeText={(input) => setNumberPostalCode(numberFormatter(input, 6))}
-        />
+                {/* Form RT*/}
+                <FloatingLabelInput
+                  label="RT" //
+                  value={numberRT}
+                  onChangeText={(input) => setNumberRT(numberFormatter(input, 3))}
+                />
 
-        {/* Form RT*/}
-        <FloatingLabelInput
-          label="RT" //
-          value={numberRT}
-          onChangeText={(input) => setNumberRT(numberFormatter(input, 3))}
-        />
+                {/* Form RW*/}
+                <FloatingLabelInput
+                  label="RW" //
+                  value={numberRW}
+                  onChangeText={(input) => setNumberRW(numberFormatter(input, 3))}
+                />
 
-        {/* Form RW*/}
-        <FloatingLabelInput
-          label="RW" //
-          value={numberRW}
-          onChangeText={(input) => setNumberRW(numberFormatter(input, 3))}
-        />
+                {/* Form Patokan*/}
+                <FloatingLabelInput
+                  label="Patokan (Opsional)" //
+                  value={benchmark}
+                  onChangeText={(input) => setBenchmark(addressFormatter(input))}
+                />
 
-        {/* Form Patokan*/}
-        <FloatingLabelInput
-          label="Patokan (Opsional)" //
-          value={benchmark}
-          onChangeText={(input) => setBenchmark(addressFormatter(input))}
-        />
+                {/* Form Alamat Lengkap*/}
+                <FloatingLabelInput
+                  label="Alamat Lengkap" //
+                  value={address}
+                  onChangeText={(input) => setAddress(addressFormatter(input))}
+                />
 
-        {/* Form Alamat Lengkap*/}
-        <FloatingLabelInput
-          label="Alamat Lengkap" //
-          value={address}
-          onChangeText={(input) => setAddress(addressFormatter(input))}
-        />
-
-        {/* Button Simpan*/}
-        <Button
-          classNameContainer={`${isDarkMode ? "bg-[#333836]" : "bg-[#159778]"} mt-4  px-6 py-2 rounded-lg items-center w-96`}
-          textClassName="text-white font-semibold text-lg" //
-        >
-          Simpan
-        </Button>
-      </View>
-    </View>
+                {/* Button Simpan*/}
+                <Button
+                  classNameContainer={`${isDarkMode ? "bg-[#333836]" : "bg-[#159778]"} mt-4  px-6 py-2 rounded-lg items-center w-96`}
+                  textClassName="text-white font-semibold text-lg" //
+                >
+                  Simpan
+                </Button>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
