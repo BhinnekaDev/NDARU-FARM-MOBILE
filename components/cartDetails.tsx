85b259332bffd55e11b294852461f4c6552de265
Adrian Musa Alfauzan <adrianmusaalfauzan@gmail.com>
2025-04-16 23:21:14 +0700

    feat: menambahka fitur baru quantity 0 = muncul delete

--- components/cartDetails.tsx
+++ components/cartDetails.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useRouter } from "expo-router";
-import { View, Image, TouchableOpacity, useColorScheme } from "react-native";
+import { View, Image, TouchableOpacity, useColorScheme, Animated } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 // COMPONENTS
 import MyText from "@/components/text";
@@ -8,13 +8,32 @@ import MyButton from "@/components/button";
 // INTERFACES
 import { MyCardProps } from "@/interfaces/cardProps";
 
-const CartDetails = ({ image, bgImageStyle, imageStyle, name, description, price, quantity, date, detailType, rating, id, onPress, buttonType = "default", isDisabled, onDelete }: MyCardProps) => {
+const CartDetails = ({
+  image, //
+  bgImageStyle,
+  imageStyle,
+  name,
+  description,
+  price,
+  quantity,
+  date,
+  detailType,
+  rating,
+  id,
+  onPress,
+  buttonType = "default",
+  isDisabled,
+  onDelete,
+}: MyCardProps) => {
   const router = useRouter();
   const [currentQuantity, setCurrentQuantity] = useState(quantity || 1);
   const [currentPrice, setCurrentPrice] = useState(price ? parseInt(price.replace("Rp", "").replace(".", "")) : 0);
   const [showDeleteButton, setShowDeleteButton] = useState(false);
   const colorScheme = useColorScheme();
   const isDarkMode = colorScheme === "dark";
+
+  // Animated values
+  const slideLeftAnim = useRef(new Animated.Value(0)).current;
 
   const onDetail = () => {
     const routes = {
@@ -66,9 +85,14 @@ const CartDetails = ({ image, bgImageStyle, imageStyle, name, description, price
       setCurrentPrice(currentPrice - numericPrice);
     }
 
-    // Jika quantity mencapai 0, tampilkan tombol delete tanpa animasi
+    // Jika quantity mencapai 0, tampilkan tombol delete dan animasi pergeseran
     if (currentQuantity === 1) {
       setShowDeleteButton(true);
+      Animated.timing(slideLeftAnim, {
+        toValue: -100, // Bergeser ke kiri
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
     }
   };
 
@@ -79,96 +103,108 @@ const CartDetails = ({ image, bgImageStyle, imageStyle, name, description, price
     setCurrentQuantity(currentQuantityNumber + 1);
     setCurrentPrice(currentPrice + numericPrice);
 
-    // Sembunyikan tombol delete saat quantity lebih dari 0
+    // Sembunyikan tombol delete saat quantity lebih dari 0 dan animasi kembali ke posisi semula
     setShowDeleteButton(false);
+    Animated.timing(slideLeftAnim, {
+      toValue: 0, // Kembali ke posisi awal
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
   };
 
   useEffect(() => {
     if (currentQuantity === 0) {
       setShowDeleteButton(true); // Menampilkan tombol delete jika quantity = 0
+      Animated.timing(slideLeftAnim, {
+        toValue: -100, // Bergeser ke kiri
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
     } else {
       setShowDeleteButton(false); // Menyembunyikan tombol delete jika quantity > 0
+      Animated.timing(slideLeftAnim, {
+        toValue: 0, // Kembali ke posisi awal
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
     }
   }, [currentQuantity]);
 
   return (
     <TouchableOpacity activeOpacity={0.8} onPress={onDetail}>
-      <View className="p-4 w-full rounded-3xl shadow-md" style={{ backgroundColor: getCardBackgroundColor() }}>
-        {/* Tombol Hapus */}
-        {showDeleteButton && onDelete && (
+      {/* Tombol Hapus */}
+      {showDeleteButton && onDelete && (
+        <View className="absolute h-full  w-16 flex items-center justify-center right-0 bg-purple-400">
+          <TouchableOpacity onPress={onDelete} style={{ padding: 10 }}>
+            <Ionicons name="trash-outline" size={32} color="white" />
+          </TouchableOpacity>
+        </View>
+      )}
+
+      {/* Animated View for Cart */}
+      <Animated.View
+        style={{
+          transform: [{ translateX: slideLeftAnim }],
+          backgroundColor: getCardBackgroundColor(),
+        }}
+        className="px-4 py-3 w-full h-44 rounded-2xl shadow-md flex-row items-center justify-between"
+      >
+        {/* Gambar Produk */}
+        <View className="w-40 h-full flex-row items-center  justify-center bg-purple-400 rounded-lg">
           <View
+            className={`w-40 h-full absolute rounded-lg ${bgImageStyle} `}
             style={{
-              position: "absolute",
-              top: 10,
-              right: 10,
-              backgroundColor: "black",
-              padding: 10,
-              borderRadius: 25,
-              zIndex: 1,
+              backgroundColor: getBgImageColor(),
             }}
-          >
-            <TouchableOpacity onPress={onDelete} style={{ padding: 10 }}>
-              <Ionicons name="trash-outline" size={44} color="white" />
-            </TouchableOpacity>
-          </View>
-        )}
+          />
+          <Image source={image} className={`w-24 h-24 rounded-xl ${imageStyle} `} resizeMode="cover" />
+        </View>
 
-        <View className="flex-row items-center">
-          {/* Gambar Produk */}
-          <View className="w-2/5 flex-row items-center h-full justify-center">
-            <View
-              className={`w-[95px] h-[95px] absolute rounded-lg ${bgImageStyle} `}
-              style={{
-                backgroundColor: getBgImageColor(),
-              }}
-            />
-            <Image source={image} className={`w-24 h-24 rounded-xl ${imageStyle} `} resizeMode="cover" />
-          </View>
+        {/* Info Produk */}
+        <View className="flex-1">
+          <MyText fontSize={16} fontFamily="LexBlack" color="white">
+            {name}
+          </MyText>
 
-          <View className="flex-1 h-full">
-            {/* Teks Nama Produk */}
-            <MyText fontSize={26} fontFamily="LexBlack" color="white">
-              {name.length > 11 ? `${name.slice(0, 11)}...` : name}
-            </MyText>
+          <MyText fontSize={13} fontFamily="LexMedium" color="gray" textstyle="mt-1">
+            {description}
+          </MyText>
 
-            {/* Teks Deskripsi Produk */}
-            <MyText fontSize={15} fontFamily="LexMedium" color="white" textstyle="ml-4">
-              {description.length > 17 ? description.slice(0, 17) + " ..." : description}
-            </MyText>
-            <MyText fontSize={15} fontFamily="LexMedium" color="white" textstyle="ml-4">
-              {rating}
-            </MyText>
-            {/* Teks Harga atau Tanggal Produk */}
-            {price ? (
-              <View className="flex-row justify-center items-center my-5">
-                <MyText fontSize={20} fontFamily="LexBlack" color="white">
-                  {`Rp${currentPrice.toLocaleString()}`}
-                </MyText>
-                <MyText fontSize={15} fontFamily="LexLight" color="white">
-                  /{currentQuantity}
-                </MyText>
-              </View>
-            ) : date ? (
-              <MyText fontSize={20} fontFamily="LexBlack" color="white" textstyle="text-center my-5">
-                {date}
+          <View className="flex-row items-center mt-1">
+            <Ionicons name="star" size={14} color="white" />
+            <MyText fontSize={13} fontFamily="LexMedium" color="white" textstyle="ml-1">
+              {rating}{" "}
+              <MyText fontSize={11} fontFamily="LexLight" color="gray">
+                (10K/Penilaian)
               </MyText>
-            ) : null}
+            </MyText>
+          </View>
 
-            {/* Tombol Kurang, Quantity, dan Tambah */}
-            <View className="flex-row items-center justify-between mt-4">
-              <TouchableOpacity onPress={decreaseQuantity} className="px-4 py-2 bg-gray-300 rounded-lg">
-                <Ionicons name="remove" size={24} color="black" />
-              </TouchableOpacity>
-              <MyText fontSize={18} fontFamily="LexMedium" color="white" textstyle="mx-4">
-                {currentQuantity}
-              </MyText>
-              <TouchableOpacity onPress={increaseQuantity} className="px-4 py-2 bg-gray-300 rounded-lg">
-                <Ionicons name="add" size={24} color="black" />
-              </TouchableOpacity>
-            </View>
+          <View className="flex-row items-center mt-1">
+            <MyText fontSize={15} fontFamily="LexBlack" color="white">
+              Rp{currentPrice.toLocaleString()}
+            </MyText>
+            <MyText fontSize={12} fontFamily="LexLight" color="gray" textstyle="ml-1">
+              x{currentQuantity}
+            </MyText>
           </View>
         </View>
-      </View>
+
+        {/* Tombol Tambah & Kurang */}
+        <View className="items-center">
+          <TouchableOpacity onPress={increaseQuantity} className="w-[32px] h-[32px] rounded-md bg-white items-center justify-center">
+            <Ionicons name="add" size={20} color="black" />
+          </TouchableOpacity>
+
+          <MyText fontSize={16} fontFamily="LexBold" color="white" textstyle="my-1">
+            {currentQuantity}
+          </MyText>
+
+          <TouchableOpacity onPress={decreaseQuantity} className="w-[32px] h-[32px] rounded-md bg-white items-center justify-center">
+            <Ionicons name="remove" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
     </TouchableOpacity>
   );
 };
